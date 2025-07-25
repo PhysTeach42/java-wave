dec64b157ffc5553b71ccaf9571cc767c8360d27
PhysTeach42 <119270116+PhysTeach42@users.noreply.github.com>
2025-07-24 20:16:45 -0500

    added 2nd function in js folder

--- jsFolder/newScript.js
+++ jsFolder/newScript.js
@@ -1,3 +1,7 @@
 function myNewFunction() {
     document.getElementById("demo").innerHTML = "new text from folder";
 }
+
+function myNextFunction() {
+    document.getElementById("demo3").innerHTML = "2nd text from folder";
+}
